785dbcdeb9fd26b94a18a0f601f312a146cdc79d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 20:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-    let element = document.createElement('div');
-    element.innerHTML = 'Hello, DOM!';
-    element.style.backgroundColor = '#f9f9f9';
+  let element = document.createElement('div');
+  element.innerHTML = 'Hello, DOM!';
+  element.style.backgroundColor = '#f9f9f9';
+  document.body.appendChild(element);
+  element.style.textAlign = 'center';
+  let ul = document.createElement('ul');
+
+  for (let i = 0; i < 3; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);
+  }
