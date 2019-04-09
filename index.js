053517307ebd86de2c733a7e9b923f5f9ff55920
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 20:34:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+  let element = document.createElement('div');
+  element.innerHTML = 'Hello, DOM!';
+  element.style.backgroundColor = '#f9f9f9';
+  document.body.appendChild(element);
+  element.style.textAlign = 'center';
+  let ul = document.createElement('ul');
+  element.appendChild(ul);
+
+  for (let i = 0; i < 3; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = (i + 1).toString();
+    ul.appendChild(li);
+  }
+  ul.style.textAlign = 'left';
+  ul.removeChild(ul.querySelector('li:nth-child(2)'));
+  ul.remove();
