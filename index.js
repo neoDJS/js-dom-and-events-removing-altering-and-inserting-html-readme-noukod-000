94e1c037e325a48567ffac83f7c310e9d4eab787
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 20:34:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
   document.body.appendChild(element);
   element.style.textAlign = 'center';
   let ul = document.createElement('ul');
+  element.appendChild(ul);
 
   for (let i = 0; i < 3; i++) {
     let li = document.createElement('li');
@@ -11,3 +12,5 @@
     ul.appendChild(li);
   }
       ul.style.textAlign = 'left';
+    ul.removeChild(ul.querySelector('li:nth-child(2)'));
+        ul.remove();
